522946f4aadadb1bd5679aa4a8e457903b763aa8
Jennifer Rosado <Jennifer@Jennifer.local>
2023-06-12 08:31:15 -0700

    fix hour colors in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,23 @@
-// function to add current date subtitle in header
-function currentDay() {
-    var currentDay = document.getElementById("currentDay");
-    currentDay.textContent = "Today is " + moment().format("dddd, MMMM Do YYYY");
-}
+// use moment.js to load current day into header
+$("#currentDay").text(`Today is ${moment().format("dddd, MMMM Do YYYY")}`);
 
 // function to determine hour textarea bg color based on current hour
 function hourColor() {
-    var currentHour = dayjs();
-    for (var i = 9; i < 18; i++) {
-        if (i < currentHour.hour()) {
+    var now = dayjs();
+    for (var i = 9; i <= 17; i++) {
+        if (i < now.hour()) {
             $("#hour-" + i).addClass("past");
-        } else if (i = currentHour.hour()) {
-            $("hour-" + i).addClass("present");
+        } else if (i = now.hour()) {
+            $("#hour-" + i).addClass("present");
         } else {
-            $("hour-" + i).addClass("future");
+            $("#hour-" + i).addClass("future");
         }
     }
 }
 
-currentDay();
 hourColor();
+
+
+$(function() {
+    hourColor();
+});
