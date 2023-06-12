ee86ebf43b76e0fc85b05ff2783898b758e56255
Jennifer Rosado <Jennifer@Jennifer.local>
2023-06-11 20:07:01 -0700

    add script for hour block colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
+// function to add current date subtitle in header
 function currentDay() {
     var currentDay = document.getElementById("currentDay");
     currentDay.textContent = "Today is " + moment().format("dddd, MMMM Do YYYY");
 }
 
+// function to determine hour textarea bg color based on current hour
+function hourColor() {
+    var currentHour = dayjs();
+    for (var i = 9; i < 18; i++) {
+        if (i < currentHour.hour()) {
+            $("#hour-" + i).addClass("past");
+        } else if (i = currentHour.hour()) {
+            $("hour-" + i).addClass("present");
+        } else {
+            $("hour-" + i).addClass("future");
+        }
+    }
+}
+
 currentDay();
+hourColor();
